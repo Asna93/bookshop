28ea49e539915b21e6916c084a5eb418d53cd4b5
Asna93 <asnasgar93@gmail.com>
2024-12-04 10:46:58 +0400

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from shop.models import Books
 from django.core.cache import cache
 
-def book_list(request):
-    book_list=cache.get('book_list')
 
-    if book_list is None:
-        book_list=list(Books.objects.all())
+   def home(request):
+    books=Books.objects.all()
+    return render(request,'home.html',{'books':books})
 
-        cache.set('book_list',book_list,900)
+def cart(request):
+    return render(request, 'cart.html')
 
-    return render(request,'home.html',{'books':book_list})
+def product_list(request):
+    product_list = cache.get('product_list')
 
-   
+    if product_list is None:
+        # If not in cache, fetch the data from the database
+        product_list = list(Books.objects.all())  # Convert QuerySet to list
+
+        # Store the product list in the cache for 15 minutes (900 seconds)
+        cache.set('product_list', product_list, 900)
+
+    return render(request, 'home.html', {'products': product_list})
